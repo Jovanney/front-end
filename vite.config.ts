622fedc65894720453ef88cda1424a868e6d9674
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-12-16 17:39:39 -0300

    feat: refactor TypeScript configuration and clean up unused imports in components

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
     },
   },
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
