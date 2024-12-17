15b445b4e8d10d176c127e32841fe98678fd8fda
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-12-17 08:39:02 -0300

    feat: update TypeScript and Vite configuration to support environment variables and improve project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+export default defineConfig((env) => {
+  const envars = loadEnv(env.mode, './')
+  const serverURL = new URL(envars.VITE_SERVER_URL ?? '<http://localhost:5032>')
+  const serverAPIPath = envars.VITE_SERVER_API_PATH ?? '/api'
+
+  return {
+    envDir: './',
+    define: {
+      __API_PATH__: JSON.stringify(serverAPIPath),
     },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
     },
-  },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+
+    server: {
+      port: 5173,
+      proxy: {
+        // proxy requests with the API path to the server
+        // <http://localhost:5173/api> -> <http://localhost:5032/api>
+        [serverAPIPath]: serverURL.origin,
+      },
+    },
+  }
 })
